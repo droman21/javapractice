0d4da52bf246fbb302736768892088c45fbf7bc4
Daniel Roman <dan.roman21@gmail.com>
2020-08-22 20:04:07 -0400

    Working on types and variables examples

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("What's Up, World!");
+
+        int num = 10;
+        String s = "I have in my collection " + num + " vinyl records";
+        System.out.println(s);
+
+        String a1 = new String("What do you want on your pizza?");
+        String a2 = new String(" I like mushrooms.");
+        String a3 = a1 + a2;
+        System.out.println(a3);
+
+    }
+
+}
